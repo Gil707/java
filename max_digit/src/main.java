029b43a31205300103c7c7dc2d55b33a28cdbd23
Gil707 <Gil707@gmail.com>
2016-10-27 22:20:19 +0300

    max_digit

---
+++ max_digit/src/main.java
@@ -0,0 +1,28 @@
+// Максимальная цифра числа
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число : ");
+        int value = sc.nextInt();
+
+        System.out.println("Максимальная цифра числа = " + maxdigit(value));
+
+    }
+    static int maxdigit(int a) {
+        int digit;
+        int value_max = 0;
+        while (a > 0)
+        {
+            digit = a%10;
+            if ( digit > value_max ) value_max=digit;
+            a = (a - digit)/10;
+        }
+        return value_max;
+
+    }
+}
