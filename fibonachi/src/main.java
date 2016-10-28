223b0e3e0b24a86b8fd1726854d45a41e10c3b8f
Gil707 <Gil707@gmail.com>
2016-10-28 17:56:01 +0300

    Числа Фибоначчи

---
+++ fibonachi/src/main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] argh) {
+
+        int a = 1;
+        int b = 1;
+        int n, i;
+        int sum_fib;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Введите кол-во чисел Фибоначчи : ");
+        n = s.nextInt();
+        System.out.print("Цикл FOR : " + a + " " + b + " ");
+        for (i = 2; i < n; i++) {
+            sum_fib = a + b;
+            a = b;
+            b = sum_fib;
+            System.out.print(sum_fib + " ");
+
+        };
+
+        System.out.println();
+
+        a = 1; b = 1;
+        System.out.print("Цикл WHILE : " + a + " " + b);
+        sum_fib = 2; i = 2;
+        while (i < n) {
+            sum_fib = a + b;
+            a = b;
+            b = sum_fib;
+            System.out.print(" " + sum_fib);
+            i++;
+        }
+    }
+}
