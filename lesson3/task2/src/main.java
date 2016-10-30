216a16809abbe6af92771950e5e02ca8bf430fc0
Gil707 <Gil707@gmail.com>
2016-10-30 10:58:25 +0300

    от 1 до 99 нечетные

--- lesson3/task2/src/main.java
+++ lesson3/task2/src/main.java
@@ -1,31 +1,37 @@
 import java.util.Scanner;
 
-public class arrays {
+public class main {
 
     public static void main(String[] args) {
+
+        int[] ar;
+        int cnt = 0;
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите нижнюю границу : ");  // первая цифра
         int a = sc.nextInt();
-        System.out.println("Введите верхнюю границу : "); // последняя цифра, максимальная
+        System.out.print("Введите верхнюю границу : "); // последняя цифра, максимальная
         int b = sc.nextInt();
 
-        for (int c = a; a < b; c++) {
-            
+        for (int c = a; c <= b; c++) {                   // пределяем количество нечетных чисел
+            if(c%2!=0) {
+                cnt++;
+            }
         }
 
+        System.out.println("Массив из " + cnt + " чисел");
 
+        ar = new int[cnt];
 
-        int[] ar = new int[];
-
-        for (int i = 0, n = 1; i < ar.length; i++, n += 2) {
+        for (int i = 0, n = a; i < ar.length; i++, n += 2) {   // выводим в строчку
             ar[i] = n;
             System.out.print(ar[i] + " ");
         }
         System.out.println();
 
-        for (int i = 0; i < ar.length; i++) {
-            System.out.println(ar[i]);
+        for (int i = ar.length-1; i >= 0; i--) {                   // в обратном порядке
+            System.out.print(ar[i] + " ");
         }
     }
 }
+
